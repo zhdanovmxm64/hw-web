4d9d5b826aaf340892250e89c6ea765efd578386
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-08-20 11:52:42 +0400

    new dir

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.views import index, contact, products, add_products
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index),
@@ -8,3 +9,4 @@ urlpatterns = [
     path('products/', products),
     path('add_products/', add_products),
 ]
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
