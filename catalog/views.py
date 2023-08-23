c5f0225c295430500568ed257a180b1c874f0dc4
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-08-23 19:26:49 +0400

    Added new page: products.html

--- catalog/views.py
+++ catalog/views.py
@@ -3,16 +3,27 @@ from catalog.models import Product, Contacts
 
 
 def index(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list
+    }
     print(Product.objects.all().order_by('-date')[:5])
-    return render(request, 'catalog/index.html')
+    return render(request, 'catalog/index.html', context)
 
 
 def contact(request):
-    contact_info = Contacts.objects.get(pk=1).__dict__
-
+    context = Contacts.objects.get(pk=1).__dict__
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contact.html', contact_info)
+    return render(request, 'catalog/contact.html', context)
+
+
+def products(request):
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list
+    }
+    return render(request, 'catalog/products.html', context)
